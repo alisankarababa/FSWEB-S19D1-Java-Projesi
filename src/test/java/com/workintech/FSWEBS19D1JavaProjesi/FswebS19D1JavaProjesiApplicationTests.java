c5e07cf1ab5e5b36cd99f44ced0fcc10ab4c3126
Alişan <alisankarababa@gmail.com>
2023-12-26 17:56:46 +0300

    init project

---
+++ src/test/java/com/workintech/FSWEBS19D1JavaProjesi/FswebS19D1JavaProjesiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workintech.FSWEBS19D1JavaProjesi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FswebS19D1JavaProjesiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
